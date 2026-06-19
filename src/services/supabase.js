c53171f7755eb8c11876022c966762dd0272e709
Yoga P. Effendi <legacyasphere@gmail.com>
2026-06-19 23:33:04 +0700

    feat: add src/services/supabase.js

---
+++ src/services/supabase.js
@@ -0,0 +1,4 @@
+import { createClient } from '@supabase/supabase-js'
+const SUPABASE_URL = 'https://eqktmdyvftuuniqoxsgz.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxa3RtZHl2ZnR1dW5pcW94c2d6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4NzUzMDcsImV4cCI6MjA5NzQ1MTMwN30.KecxrBRZKCJt9zMSvYI4Gr5tKUKb_tYLofsuPNc7X64'
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
